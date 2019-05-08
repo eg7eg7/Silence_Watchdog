2269e6dd5253ea1f9e1be99c5ccd81b64ce4f386
eg7eg7 <eg7eg7@gmail.com>
2019-05-08 15:05:43 +0300

    seek bar listener

--- app/src/main/java/com/example/silencewatchdog/WatchdogMainActivity.java
+++ app/src/main/java/com/example/silencewatchdog/WatchdogMainActivity.java
@@ -7,23 +7,49 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class WatchdogMainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner mode_selector;
+    private Button ToggleStartStopButton;
+    private Button soundControlBtn;
+    private ArrayAdapter<CharSequence> adapter;
+    private SeekBar threshold_Seeker;
+    private TextView threshold_indicator_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watchdog_main);
 
-        Spinner spinner = findViewById(R.id.silence_mode_id);
-        Button soundControlBtn = findViewById(R.id.soundControllerBtn);
-        final Button startBtn = findViewById(R.id.startBtn);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.modes, android.R.layout.simple_spinner_item);
+        mode_selector = findViewById(R.id.silence_mode_id);
+        soundControlBtn = findViewById(R.id.soundControllerBtn);
+        ToggleStartStopButton = findViewById(R.id.startBtn);
+        adapter = ArrayAdapter.createFromResource(this, R.array.modes, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        mode_selector.setAdapter(adapter);
+        mode_selector.setOnItemSelectedListener(this);
+        threshold_Seeker = findViewById(R.id.seekbar_id);
+        threshold_indicator_text = findViewById(R.id.threshold_indicator_id);
 
+
+        threshold_Seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                //set textView's text
+                threshold_indicator_text.setText(""+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+
+        });
         soundControlBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,13 +57,13 @@ public class WatchdogMainActivity extends AppCompatActivity implements AdapterVi
                 startActivity(soundControllerIntent);
             }
         });
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        ToggleStartStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startBtn.getText().equals("Start"))
-                    startBtn.setText("Stop");
+                if(ToggleStartStopButton.getText().equals("Start"))
+                    ToggleStartStopButton.setText("Stop");
                 else
-                    startBtn.setText("Start");
+                    ToggleStartStopButton.setText("Start");
             }
         });
     }
