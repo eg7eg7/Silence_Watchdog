ef9b988f87472c69d2692775da8d777a4263613b
eg7eg7 <eg7eg7@gmail.com>
2019-05-08 11:27:50 +0300

    Changed project name, some GUI modifications

---
+++ app/src/main/java/com/example/silencewatchdog/SoundControllerActivity.java
@@ -0,0 +1,42 @@
+package com.example.silencewatchdog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class SoundControllerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_controller);
+
+        Spinner spinner = findViewById(R.id.soundSpinner);
+        Button backBtn = findViewById(R.id.backBtn);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.soundModes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
