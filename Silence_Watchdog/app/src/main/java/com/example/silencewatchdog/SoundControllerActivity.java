7d007766ecbdb635ca7d9d1010777a8e46fbb8c7
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-05-16 17:24:08 +0300

    Shhhh WORKING!!!!!

--- Silence_Watchdog/app/src/main/java/com/example/silencewatchdog/SoundControllerActivity.java
+++ Silence_Watchdog/app/src/main/java/com/example/silencewatchdog/SoundControllerActivity.java
@@ -1,9 +1,12 @@
 package com.example.silencewatchdog;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,16 +16,19 @@ import android.widget.Spinner;
 public class SoundControllerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner sound_selector;
     private Button backBtn;
+    private Button playBtn;
     private ArrayAdapter<CharSequence> adapter;
     private SharedPreferences preferences;
+    private MediaPlayer soundTest;
     Editor prefEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_controller);
-
+        soundTest = MediaPlayer.create(this, R.raw.shhh);
         sound_selector = findViewById(R.id.soundSpinner);
         backBtn = findViewById(R.id.backBtn);
+        playBtn = findViewById(R.id.playBtn);
         adapter = ArrayAdapter.createFromResource(this, R.array.soundModes, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sound_selector.setAdapter(adapter);
@@ -36,6 +42,12 @@ public class SoundControllerActivity extends AppCompatActivity implements Adapte
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                soundTest.start();
             }
         });
     }
